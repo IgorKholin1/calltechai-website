30909de060f2030387eb7f844cc42a228e3976bf
IgorKholin1 <igorkholin89@gmail.com>
2025-06-09 17:45:35 -0700

    Create token.js

---
+++ routes/token.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const twilio = require('twilio');
+const router = express.Router();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const apiKey = process.env.TWILIO_API_KEY_SID;
+const apiSecret = process.env.TWILIO_API_KEY_SECRET;
+const appSid = process.env.TWILIO_TWIML_APP_SID;
+
+router.get('/token', (req, res) => {
+  const AccessToken = twilio.jwt.AccessToken;
+  const VoiceGrant = AccessToken.VoiceGrant;
+
+  const token = new AccessToken(accountSid, apiKey, apiSecret, { ttl: 3600 });
+  const voiceGrant = new VoiceGrant({ outgoingApplicationSid: appSid });
+  token.addGrant(voiceGrant);
+  token.identity = 'browser-user';
+
+  res.send({ token: token.toJwt() });
+});
+
+module.exports = router;
